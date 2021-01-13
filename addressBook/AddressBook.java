1acb70800b6fe2dcb897c4a0691efa6e8b240ab8
nitinkandikatla <nitinkandikatla99@gmail.com>
2021-01-13 11:40:13 +0530

    ADDED : added new contact in address book

--- addressBook/AddressBook.java
+++ addressBook/AddressBook.java
@@ -1,10 +1,57 @@
 package addressBook;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
 
 	public static void main(String[] args) {
-		Person p=new Person("rahul","patil","pune","maharashtra",413006,9988321);
-		System.out.println(p);
-
+		int choice=1,person_count=0;
+	    ArrayList<Person> person = new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+		while (choice!=0)
+        {
+            System.out.println("1.Add Person \n 2.Display \n ");
+            System.out.println("Enter your choice:");
+            choice=sc.nextInt();
+            switch (choice)
+            {
+                case 1:
+                	
+                    System.out.println("Enter Number of persons:");
+                    person_count=sc.nextInt();
+                    Person p[]=new Person[person_count];
+                    for(int i=0;i<person_count;i++)
+                    {
+                    	p[i]=new Person();
+                    	
+                    	Scanner s=new Scanner(System.in);
+                    	System.out.println("Enter First name :");
+                    	p[i].firstName=s.nextLine();
+                    	System.out.println("Enter last name :");
+                    	p[i].lastName=s.nextLine();
+                    	System.out.println("Enter city :");
+                    	p[i].city=s.nextLine();
+                    	System.out.println("Enter state :");
+                    	p[i].state=s.nextLine();
+                    	System.out.println("Enter zip :");
+                    	p[i].zip=s.nextInt();
+                    	System.out.println("Enter phone number :");
+                    	p[i].phoneNum=s.nextLine();
+                    	person.add(p[i]);
+                    	
+                    }
+                    break;
+                  
+                case 2:
+                	for(int i=0;i<person_count;i++)
+                	{	
+                		System.out.println(person);
+                		break;
+                	}
+                default:
+                	System.exit(0);
+               }
+		
+		}
 	}
-
 }
